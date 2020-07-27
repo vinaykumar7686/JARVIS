720522a9bf4bc61cf3db21419cc7055f1381eed3
Vinay <vinaykumarvk.8866@gmail.com>
2020-07-27 22:53:38 +0530

    Added float value functionality to preprocessing method

--- arithmetics.py
+++ arithmetics.py
@@ -1,3 +1,10 @@
+def isPartialFloat(s):
+    try:
+        float(s)
+        return True
+    except Exception:
+        return False
+
 def convert(exp):
     exp.append('$')
     point = 0
@@ -8,9 +15,14 @@ def convert(exp):
         if sym.isdigit():
             if exp[i-1].isdigit():
                 res[point-1] = res[point-1]+sym
+            elif res and isPartialFloat(res[point-1]):
+                res[point-1] = res[point-1]+sym
             else:
                 res.append(sym)
                 point+=1
+        elif sym=='.':
+            if exp[i-1].isdigit():
+                res[point-1] = res[point-1]+sym
         elif sym.isalpha():
             if exp[i-1].isalpha():
                 res[point-1] = res[point-1]+sym
